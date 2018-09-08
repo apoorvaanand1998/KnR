e1c583eb948f3b204fbc5b8872d5d3afdc8fc53e
Apoorva Anand <mitul.apoorva@gmail.com>
2018-09-08 20:59:56 +0530

    Remove comments

---
+++ ex1_23.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main()
+{
+  int c, cp, i, singlelinec, multilinec, commentpossible, justleftmlc;
+  char buf[1000];
+  
+  singlelinec = 0;
+  multilinec = 0;
+  commentpossible = 0;
+  justleftmlc = 0;
+  cp = -1;
+  i = 0;
+
+  while ((c = getchar()) != EOF) {
+    justleftmlc = 0;
+    
+    if (singlelinec == 1) {
+      if (c == '\n') {
+	singlelinec = 0;
+      }
+    }
+    if (multilinec == 1) {
+      if ((cp == '*') && (c == '/')) {
+	multilinec = 0;
+	justleftmlc = 1;
+      }
+    }
+    
+    if ((cp == '/') && (c == '/')) {
+      singlelinec = 1;
+      commentpossible = 0;
+    }
+    if ((cp == '/') && (c == '*')) {
+      multilinec = 1;
+      commentpossible = 0;
+    }
+
+    if ((singlelinec == 0) && (multilinec == 0)) {
+      if ((commentpossible == 1) && (c != '/')) {
+	buf[i++] = cp;
+	buf[i++] = c;
+	commentpossible = 0;
+      }
+      else if (c != '/') {
+	buf[i++] = c;
+      }
+      else if (justleftmlc == 0) {
+	commentpossible = 1;
+      }
+    }
+    cp = c;
+  }
+  buf[i] = '\0';
+  printf("%s", buf);
+  return 0;
+}
