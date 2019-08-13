02adacc1c136e9635c943cba5701688289113d13
Apoorva Anand <apoorvaanand.work@gmail.com>
2019-08-13 08:22:03 +0530

    Finish ex 2_6

---
+++ ex2_6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+unsigned setbits(x, p, n, y)
+{
+  mask = ~(~0 << n); //sets the last n bits to 1
+  //mask off p to p + n bits of x OR'd with mask on only the last n digits of y and move them to the right position i.e p to p + n
+  return ((x & ~(mask << p + 1 - n)) | (y & mask) << p + 1 - n);
+}
