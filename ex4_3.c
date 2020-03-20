da7dce0df7723b3aa513729834bb567a18a5fcc1
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-03-20 16:33:41 +0530

    Solve 4_3

--- ex4_3.c
+++ ex4_3.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
 
 #define MAXSTACK 100
 #define BUFSIZE 100
+#define MAXOP 50
+#define NUMBER '0'
 
 double stack[MAXSTACK];
 char buf[BUFSIZE];
@@ -35,7 +40,7 @@ int getch(void)
   return (buf_pos > 0) ? buf[--buf_pos] : getchar();
 }
 
-int ungetch(int c)
+void ungetch(int c)
 {
   if (buf_pos >= BUFSIZE) {
     printf("Error : BUFFER FULL\n");
@@ -43,5 +48,86 @@ int ungetch(int c)
   else {
     buf[buf_pos++] = c;
   }
+}
+
+int getop(char s[])
+{
+  int c, d, i;
+
+  while ((c = s[0] = getch()) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+
+  if (!isdigit(c) && c != '.' && c != '-')
+    return c;
+
+  i = 0;
+  if (c == '-') {
+    if ((d = getch()) == ' ')
+      return c;
+    else {
+      s[++i] = d;
+      c = d;
+    }
+  }
+
+  if (isdigit(c))
+    while (isdigit((c = s[++i] = getch())))
+      ;
+  if (c == '.')
+    while (isdigit((c = s[++i] = getch())))
+      ;
+
+  s[i] = '\0';
+
+  if (c != EOF)
+    ungetch(c);
+  return NUMBER;
+}
+
+int main()
+{
+  char s[MAXOP];
+  int type;
+  double op2;
+  
+  while ((type = getop(s)) != EOF) {
+    switch (type) {
+    case NUMBER:
+      push(atof(s));
+      break;
+    case '+':
+      push(pop() + pop());
+      break;
+    case '*':
+      push(pop() * pop());
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '/':
+      op2 = pop();
+      if (op2 == 0)
+	printf("Error : DIVISION BY ZERO");
+      else
+	push(pop() / op2);
+      break;
+    case '%':
+      op2 = pop();
+      if (op2 == 0)
+	printf("Error : MODULUS WITH ZERO");
+      else
+	push(fmod(pop(), op2));
+      break;
+    case '\n':
+      printf("\t%.8g\n", pop());
+      break;
+    default:
+      printf("Error : INVALID ENTRY");
+      break;
+    }
+  }
+  return 0;
 }
   
