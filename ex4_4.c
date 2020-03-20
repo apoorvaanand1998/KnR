b45ab7c697a6ef9a1da2b9b1efb8d645747d81a2
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-03-20 17:32:03 +0530

    Solve 4_4 PROPERLY

--- ex4_4.c
+++ ex4_4.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
 
 #define MAXSTACK 100
+#define BUFSIZE 100
+#define MAXOP 50
+#define NUMBER '0'
 
 double stack[MAXSTACK];
+char buf[BUFSIZE];
+
+int buf_pos = 0;
 int stack_pos = 0;
 
 void push(double el)
@@ -60,4 +69,121 @@ void clear(void)
   stack_pos = 0;
   stack[stack_pos] = '\0';
 }
+
+int getch(void)
+{
+  return (buf_pos > 0) ? buf[--buf_pos] : getchar();
+}
+
+void ungetch(int c)
+{
+  if (buf_pos >= BUFSIZE) {
+    printf("Error : BUFFER FULL\n");
+  }
+  else {
+    buf[buf_pos++] = c;
+  }
+}
+
+int getop(char s[])
+{
+  int c, d, i;
+
+  while ((c = s[0] = getch()) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+
+  if (!isdigit(c) && c != '.' && c != '-')
+    return c;
+
+  i = 0;
+  if (c == '-') {
+    if ((d = getch()) == ' ')
+      return c;
+    else {
+      s[++i] = d;
+      c = d;
+    }
+  }
+
+  if (isdigit(c))
+    while (isdigit((c = s[++i] = getch())))
+      ;
+  if (c == '.')
+    while (isdigit((c = s[++i] = getch())))
+      ;
+
+  s[i] = '\0';
+
+  if (c != EOF)
+    ungetch(c);
+  return NUMBER;
+}
+
+int main()
+{
+  char s[MAXOP];
+  int type, ignore_newline = 0;
+  double op2;
+  
+  while ((type = getop(s)) != EOF) {
+    switch (type) {
+    case NUMBER:
+      push(atof(s));
+      break;
+    case '+':
+      push(pop() + pop());
+      break;
+    case '*':
+      push(pop() * pop());
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '/':
+      op2 = pop();
+      if (op2 == 0)
+	printf("Error : DIVISION BY ZERO");
+      else
+	push(pop() / op2);
+      break;
+    case '%':
+      op2 = pop();
+      if (op2 == 0)
+	printf("Error : MODULUS WITH ZERO");
+      else
+	push(fmod(pop(), op2));
+      break;
+    case '>':
+      printf("%f\n", top());
+      ignore_newline = 1;
+      break;
+    case '<':
+      duplicate();
+      ignore_newline = 1;
+      break;
+    case '@':
+      swap();
+      ignore_newline = 1;
+      break;
+    case '!':
+      clear();
+      ignore_newline = 1;
+      break;
+    case '\n':
+      if (ignore_newline)
+	ignore_newline = 0;
+      else
+	printf("\t%.8g\n", pop());
+      break;
+    default:
+      printf("Error : INVALID ENTRY");
+      break;
+    }
+  }
+  return 0;
+}
+
+
 
