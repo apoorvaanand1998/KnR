e6524ee6ac753b33c9d825f0300b00aa2af39566
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-04-18 13:56:45 +0530

    Grammar

--- ex5_12_detab.c
+++ ex5_12_detab.c
@@ -13,7 +13,7 @@ int decide_mn(int argc, char *argv[], int *m, int *n)
     while (--argc > 0) {
       // current character array
       curr_ca = *++argv;
-      // if first character character array is '+'
+      // if first character of character array is '+'
       if (*curr_ca == '+') {
 	*n = atoi(curr_ca+1);
       }
