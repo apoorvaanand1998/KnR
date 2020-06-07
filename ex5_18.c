7efab4738105ca977c583afee302d275de505644
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-06-07 13:36:28 +0530

    FINALLY FINISH 5_18. ADDED ONLY A FEW LINES BUT MAN, THEY WERE HARD TO FIGURE OUT

--- ex5_18.c
+++ ex5_18.c
@@ -1,4 +1,17 @@
+/*
+   This exercise (to me) is deceptively hard. There is
+   ambiguity in the question itself. Initially, my idea
+   was to add a stack to check for unbalanced parentheses,
+   but after running the program in the book with input 
+   errors, I realized most input errors are actually 
+   covered by the K&R program, but every time an error is
+   "caught" it isn't "handled" properly, which affects the 
+   output of the next line (the next expression to be eva-
+   luated, whether right or wrong). My modifications below
+   are an attempt to "handle" the errors properly.
+*/
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -66,20 +79,32 @@ void dcl(void)
 
 void dirdcl(void)
 {
-  int type;
+  int type, error = 0;
   char temp[MAXTOKEN];
 
   if (tokentype == '(') {
     dcl();
-    if (tokentype != ')')
+    if (tokentype != ')') {
+      error = 1;
       printf("Error : Missing parentheses\n");
+    }
   }
   else if (tokentype == NAME) 
     strcpy(name, token);
-  else
+  else {
     printf("Error : Expression is neither a name nor a '(dcl)'\n");
-
-  while (((type = gettoken()) == PARENS) || (type == BRACKETS)) {
+  }
+  /* 
+     added (error != 1) below because when there'd
+     be an error because of a missing closing par-
+     enthesis, the gettoken() below would read the
+     tokens of the NEXT line or tokens of the cur-
+     rent line in a way it should not, and the next
+     line would produce (wrong) output for the 
+     previous line as well as itself. This check 
+     fixes that.
+  */
+  while ((error != 1) && ((type = gettoken()) == PARENS) || (type == BRACKETS)) {
     if (type == PARENS)
       strcat(out, "function returning ");
     else if (type == BRACKETS) {
@@ -93,12 +118,31 @@ void dirdcl(void)
 int main(void)
 {
   while (gettoken() != EOF) {
-    strcpy(datatype, token);
-    out[0] = '\0';
-    dcl();
-    if (tokentype != '\n')
-      printf("Syntax Error\n");
-    printf("%s : %s%s\n", name, out, datatype);
+    if (tokentype == NAME) {
+      /* 
+	 the above while loop has been added to 
+	 recover from the "Syntax Error" that will
+	 occur in case the tokentype != '\n',
+	 in which case, gettoken() runs again and
+	 again until the tokentype == NAME 
+	 (i.e. until the first token is a datatype 
+	 and not some other character or tokentype.
+      */
+      strcpy(datatype, token);
+      out[0] = name[0] = '\0';
+      /*
+	 added name[0] here because if there was a
+	 missing name, the output would be almost
+	 right (for the case of missing name) but 
+	 it would reuse the name from the previous
+	 line. This prevents that.
+      */
+      dcl();
+      if (tokentype != '\n') {
+	printf("Syntax Error\n");
+      }
+      printf("%s : %s%s\n", name, out, datatype);
+    }
   }
   return 0;
 }
