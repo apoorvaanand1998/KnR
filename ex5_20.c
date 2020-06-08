22c62e347256eb7685c3ecf131abc3bce611fc09
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-06-08 14:53:09 +0530

    Answer 5_20 (cheekily)

--- ex5_20.c
+++ ex5_20.c
@@ -1,15 +1,18 @@
 /*
-   This exercise (to me) is deceptively hard. There is
-   ambiguity in the question itself. Initially, my idea
-   was to add a stack to check for unbalanced parentheses,
-   but after running the program in the book with input 
-   errors, I realized most input errors are actually 
-   covered by the K&R program, but every time an error is
-   "caught" it isn't "handled" properly, which affects the 
-   output of the next line (the next expression to be eva-
-   luated, whether right or wrong). My modifications below
-   are an attempt to "handle" the errors properly.
+  I have taken lots of libery with this exercise.
+  The exercise tells us to extend dcl so it can 
+  handle functions with argument types and qualifiers.
+  This seems tedious. One would have to make the program
+  look for keywords to decide which path to take in the
+  program.
+
+  For this exercise, I've decided to concentrate on the
+  "so on" part of the exercise and have implemented a very
+  simple return value checker, so obviously incorrect decl-
+  arations such as "array of arrays" or "function returning
+  array" etc are pointed out as incorrect.
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -94,16 +97,7 @@ void dirdcl(void)
   else {
     printf("Error : Expression is neither a name nor a '(dcl)'\n");
   }
-  /* 
-     added (error != 1) below because when there'd
-     be an error because of a missing closing par-
-     enthesis, the gettoken() below would read the
-     tokens of the NEXT line or tokens of the cur-
-     rent line in a way it should not, and the next
-     line would produce (wrong) output for the 
-     previous line as well as itself. This check 
-     fixes that.
-  */
+
   while ((error != 1) && ((type = gettoken()) == PARENS) || (type == BRACKETS)) {
     if (type == PARENS)
       strcat(out, "function returning ");
@@ -115,33 +109,52 @@ void dirdcl(void)
   }
 }
 
+// returns a pointer to second word after the current word in out
+char *two_words(char *p)
+{
+  int space_counter = 0;
+
+  while (space_counter < 2) {
+    if (*p++ == ' ') {
+      ++space_counter;
+    }
+  }
+  return p;
+}
+  
+void output_return_checker()
+{
+  char *p, *op;
+
+  p = out;
+  
+  while (*p != '\0') {
+    if ((strncmp(p, "function", 8) == 0) ||
+	(strncmp(p, "array", 5) == 0)) {
+      op = two_words(p);
+      if ((strncmp(op, "pointer", 7) == 0) || (*op == '\0'))
+	printf("Seems to be a correct declaration\n");
+      else {
+	printf("Seems to be an incorrect declaration\n");
+	break;
+      }
+    }
+    ++p;
+  }
+}
+
 int main(void)
 {
   while (gettoken() != EOF) {
     if (tokentype == NAME) {
-      /* 
-	 the above if condition has been added to 
-	 recover from the "Syntax Error" that will
-	 occur in case the tokentype != '\n',
-	 in which case, gettoken() runs again and
-	 again until the tokentype == NAME 
-	 (i.e. until the first token is a datatype 
-	 and not some other character or tokentype.
-      */
       strcpy(datatype, token);
       out[0] = name[0] = '\0';
-      /*
-	 added name[0] here because if there was a
-	 missing name, the output would be almost
-	 right (for the case of missing name) but 
-	 it would reuse the name from the previous
-	 line. This prevents that.
-      */
       dcl();
       if (tokentype != '\n') {
 	printf("Syntax Error\n");
       }
       printf("%s : %s%s\n", name, out, datatype);
+      output_return_checker();
     }
   }
   return 0;
